c96f0df15b13cbf8bd1c79993b20e78b15d90bb6
aMelonRind <kong49123@gmail.com>
2024-08-16 20:42:59 +0800

    fix shulker lid

---
+++ src/main/java/io/github/amelonrind/meloutils/mixin/MixinShulkerBoxBlockEntity.java
@@ -0,0 +1,24 @@
+package io.github.amelonrind.meloutils.mixin;
+
+import net.minecraft.block.entity.ShulkerBoxBlockEntity;
+import net.minecraft.block.entity.ShulkerBoxBlockEntity.AnimationStage;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ShulkerBoxBlockEntity.class)
+public class MixinShulkerBoxBlockEntity {
+
+    @Shadow private AnimationStage animationStage;
+
+    @Inject(method = "onSyncedBlockEvent", at = @At(value = "FIELD", opcode = Opcodes.PUTFIELD, target = "Lnet/minecraft/block/entity/ShulkerBoxBlockEntity;viewerCount:I"))
+    public void onBlockSync(int type, int data, CallbackInfoReturnable<Boolean> cir) {
+        if (data > 1) {
+            animationStage = AnimationStage.OPENING;
+        }
+    }
+
+}
